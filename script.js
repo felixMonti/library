126a82037c675ba31348ff98cfbdcd96c0ae345a
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-11-23 20:49:06 -0300

    Grabbing DOM elements

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function Book(title,author,pages,read,picture){
         return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
     }
 }
+ 
+
+document.addEventListener('DOMContentLoaded',function(){
+
+    const title = document.getElementById('book-title');
+    const author = document.getElementById('book-author');
+    const pages = document.getElementById('book-pages');
+    const status = document.getElementById('read-status');
+    const bookCover = document.getElementById('fileInput');
+
+    const addBook = document.querySelector('.add');
+    const close = document.querySelector('.close');
+    const modal = document.querySelector('.modal');
+    const submit = document.querySelector('.submit');
+    const form = document.getElementById('form');
+
+})
