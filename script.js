3e6a6a60e1f86e125acb3d671cc128a21a09699a
felixMonti <175974086+felixMonti@users.noreply.github.com>
2024-11-23 18:01:30 -0300

    book constructor added to script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+function Book(title,author,pages,read,picture){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.picture=picture;
+    this.read=read;
+    
+    this.info= function(){
+        return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+    }
+}
